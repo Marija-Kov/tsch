abf13a9787f93eaeebf6fb83418c6997b01080b5
marija <kccmrj@outlook.com>
2026-01-22 19:10:08 +0100

    update and add new types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,49 +1,49 @@
 export type FormInputData = {
-    from: StationName | undefined;
-    to: StationName | undefined;
-    date: YyyyMmDd | undefined;
-    time: TimeOutput | undefined;
+  from: StationName | undefined;
+  to: StationName | undefined;
+  date: YyyyMmDd | undefined;
+  time: TimeOutput | undefined;
 };
 
 export type DepartureProps = DepartureOutput;
 
 export type DepartureDetails = DepartureProps & {
-    from: StationNameFormatted;
-    to: StationNameFormatted;
+  from: StationNameDisplay;
+  to: StationNameDisplay;
 };
 // TODO: Ideally, there should be one FormProps that works for all repos.
 // FormPropsAPIResponse shouldn't have to exist.
 
 export type FormProps = {
-    handleSetDepartures: (d: DepartureDetails[]) => void;
+  handleSetDepartures: (d: DepartureDetails[]) => void;
 };
 /**
  * Used when data is fetched from the API.
  */
 export type FormPropsAPIResponse = {
-    handleSetDepartures: (d: DeparturesResponseBody) => void;
+  handleSetDepartures: (d: DeparturesResponseBody) => void;
 };
 
 export type InfoProps = {
-    toggleAppInfoVisibility: () => void;
+  toggleAppInfoVisibility: () => void;
 };
 
 export type DeparturesResponseBody = {
-    departureStation: StationNameFormatted;
-    arrivalStation: StationNameFormatted;
-    departures: DepartureOutput[];
+  departureStation: StationNameDisplay;
+  arrivalStation: StationNameDisplay;
+  departures: DepartureOutput[];
 } | { error: string };
 
 export type DepartureOutput = {
-    departureTime: TimeOutput;
-    arrivalTime: TimeOutput;
-    trainId: TrainIdDirection1 | TrainIdDirection2;
+  departureTime: TimeOutput;
+  arrivalTime: TimeOutput;
+  trainId: TrainIdBatajnicaOvca | TrainIdOvcaBatajnica;
 };
 
 export type StationDetails = {
-    name: StationName;
-    nameFormatted: StationNameFormatted;
-    departures: StationDepartureDetails[];
+  name: StationName;
+  nameDisplay: StationNameDisplay;
+  departures: StationDepartureDetails[];
 };
 /**
  * Details of a departure relative to a station.
@@ -51,13 +51,13 @@ export type StationDetails = {
 export type StationDepartureDetails = { time: number; trainDetails: TrainDetails };
 
 export type TrainDetails = {
-    id: TrainIdDirection1 | TrainIdDirection2;
-    directionId: DirectionId<
-        TrainIdDirection1,
-        TrainIdDirection2,
-        TrainDetails["id"]
-    >;
-    activeOnWeekendsAndHolidays: boolean | "w&h_only";
+  id: TrainIdBatajnicaOvca | TrainIdOvcaBatajnica;
+  directionId: DirectionId<
+    TrainIdBatajnicaOvca,
+    TrainIdOvcaBatajnica,
+    TrainDetails["id"]
+  >;
+  serviceFrequency: "ed" | "wd" | "wh";
 };
 
 export type DirectionId<T1, T2, Id extends T1 | T2> = Id extends T1 ? 1 : 2;
@@ -67,13 +67,13 @@ export type TrainItinerary = { station: StationName; time: number }[]; // TODO: 
 export type Train = TrainDetails & { itinerary: TrainItinerary };
 
 export type TrainsMap = {
-    [key in TrainIdDirection1 | TrainIdDirection2]: Train;
+  [key in TrainIdBatajnicaOvca | TrainIdOvcaBatajnica]: Train;
 };
 
 export type Station = {
-    name: StationName;
-    nameFormatted: StationNameFormatted;
-    departures: StationDepartureDetails[];
+  name: StationName;
+  nameDisplay: StationNameDisplay;
+  departures: StationDepartureDetails[];
 };
 
 export type YyyyMmDd = `${Year}-${Month}-${Day}`;
@@ -101,9 +101,29 @@ export type StationName =
   | "krnjaca most"
   | "krnjaca ukr"
   | "sebes"
-  | "ovca";
+  | "ovca"
+  | "rakovica"
+  | "knezevac"
+  | "kijevo"
+  | "resnik"
+  | "lazarevac"
+  | "vreoci"
+  | "stepojevac"
+  | "leskovac kolubarski"
+  | "veliki borak"
+  | "barajevo centar"
+  | "barajevo ukr"
+  | "bela reka"
+  | "mladenovac"
+  | "ripanj"
+  | "ripanj kolonija"
+  | "ripanj tunel"
+  | "klenje"
+  | "ralja"
+  | "sopot kosmajski"
+  | "vlasko polje";
 
-export type StationNameFormatted =
+export type StationNameDisplay =
   | "Batajnica"
   | "Kamendin"
   | "Zemunsko polje"
@@ -118,78 +138,68 @@ export type StationNameFormatted =
   | "Krnjača most"
   | "Krnjača ukr."
   | "Sebeš"
-  | "Ovča";
+  | "Ovča"
+  | "Rakovica"
+  | "Kneževac"
+  | "Kijevo"
+  | "Resnik"
+  | "Lazarevac"
+  | "Vreoci"
+  | "Stepojevac"
+  | "Leskovac Kolubarski"
+  | "Veliki Borak"
+  | "Barajevo Centar"
+  | "Barajevo ukr"
+  | "Bela reka"
+  | "Mladenovac"
+  | "Ripanj"
+  | "Ripanj kolonija"
+  | "Ripanj tunel"
+  | "Klenje"
+  | "Ralja"
+  | "Sopot Kosmajski"
+  | "Vlaško Polje";
 
-export type TrainIdDirection1 =
-  | 7101
-  | 7901
-  | 8001
-  | 8003
-  | 8201
-  | 8005
-  | 8007
-  | 8009
-  | 8011
-  | 8013
-  | 8015
-  | 8203
-  | 8017
-  | 8019
-  | 8021
-  | 8023
-  | 8025
-  | 8027
-  | 8029
-  | 8031
-  | 7113
-  | 8033
-  | 8035
-  | 7905
-  | 8037
-  | 8039
-  | 8041
-  | 8043
-  | 8045;
 
-export type TrainIdDirection2 =
-  | 8000
-  | 7900
-  | 8002
-  | 8004
-  | 8006
-  | 8008
-  | 8010
-  | 8012
-  | 8200
-  | 8014
-  | 8310
-  | 8016
-  | 8018
-  | 8020
-  | 8022
-  | 8024
-  | 8026
-  | 7108
-  | 8028
-  | 8030
-  | 8032
-  | 7110
-  | 8034
-  | 8036
-  | 8038
-  | 8040
-  | 8042
-  | 8202
-  | 8044
-  | 7114
-  | 8046
-  | 8048
-  | 7116
-  | 8204
-  | 8050
-  | 8340;
+export type TrainIdBatajnicaOvca =
+  7901 | 7101 | 8001 | 8003 | 8005 | 8007 | 8009 |
+  8011 | 8013 | 8015 | 8017 | 8019 | 8021 | 8023 |
+  8025 | 8027 | 8029 | 8031 | 8033 | 8035 | 8037 |
+  8039 | 8041 | 8043 | 8045;
 
-export type Year = "2025";
+export type TrainIdOvcaBatajnica =
+  8000 | 8002 | 8004 | 8006 | 8008 | 8010 | 8012 |
+  8014 | 8016 | 8310 | 8018 | 8020 | 8022 | 8024 |
+  8026 | 8028 | 8030 | 8032 | 8034 | 8036 | 8038 |
+  8040 | 8042 | 8202 | 8044 | 8046 | 8048 | 7116 |
+  8050 | 8340;
+
+export type TrainIdZemunResnik = 7901 | 7101 | 8205 | 8207 | 8209;
+
+export type TrainIdResnikZemun = 8307 | 8311 | 8202 | 7116;
+
+export type TrainIdLazarevacResnikOvca =
+  8301 | 7102 | 7902 | 8303 | 7104 | 8305 | 8307 |
+  8309 | 8311 | 8315 | 8317 | 8319 | 8321 | 8323 |
+  8325 | 8327 | 8329 | 8331 | 8333 | 7112 | 8335 |
+  8202 | 7904 | 8337 | 7116 | 8339;
+
+export type TrainIdOvcaResnikLazarevac =
+  7901 | 7101 | 7103 | 8300 | 8302 | 8304 | 8306 |
+  8308 | 8310 | 8205 | 8312 | 8207 | 8314 | 8316 |
+  8318 | 8320 | 8322 | 7111 | 8209 | 7903 | 8324 |
+  8326 | 8328 | 7115 | 8332 | 8334 | 8336 | 8338 |
+  8340;
+
+export type TrainIdOvcaZemunLazarevac = 7101 | 7103 | 7111 | 7115;
+
+export type TrainIdLazarevacZemunOvca = 7102 | 7104 | 7112 | 7116;
+
+export type TrainIdOvcaZemunMladenovac = 7901 | 7903;
+
+export type TrainIdMladenovacZemunOvca = 7902 | 7904;
+
+export type Year = "2026";
 
 export type Month =
   | "01"
@@ -238,7 +248,7 @@ export type Day =
   | "30"
   | "31";
 
-  export type Hours =
+export type Hours =
   | "00"
   | "01"
   | "02"
